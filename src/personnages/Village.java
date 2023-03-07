ac1604190b650445b556583bdbe2b8230f0f5e53
Damien Bonzom <bnd4424a@ens.fsi>
2023-03-07 15:48:42 +0100

    Question 1a

--- src/personnages/Village.java
+++ src/personnages/Village.java
@@ -1,11 +1,21 @@
 package personnages;
 
 public class Village {
+	
+	private String nom;
+	private Chef chef;
+	private int nbVillageois = 0;
+	private Gaulois[] villageois;
+	
+	public Village(String nom) {
+		this.nom = nom;
+	}
+	
+	public void setChef(Chef chef) {
+		this.chef = chef;
+	}
+	
 	public String getNom() {
 		return nom;
 	}
-
-	private String nom;
-	
-	
 }
