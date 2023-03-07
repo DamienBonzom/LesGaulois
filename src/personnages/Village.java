e61e096056967fa8151bf3993dc085a33912e964
Damien Bonzom <bnd4424a@ens.fsi>
2023-03-07 16:06:14 +0100

    Question 1e

--- src/personnages/Village.java
+++ src/personnages/Village.java
@@ -29,4 +29,21 @@ public class Village {
 		return villageois[numero_villageois];
 	}
 	
+	public static void main(String[] args) {
+		Village village = new Village("Village des Irréductibles", 30);
+		// Gaulois gaulois = village.trouverHabitant(30);
+		// On a en exception "ArrayIndexOutOfBoundsExcepetion" car il l'indice maximum de la liste est 29
+		
+		Chef chef_village = new Chef("Abraracourcix", 6, village);
+		village.setChef(chef_village);
+		
+		Gaulois asterix = new Gaulois("Asterix", 8);
+		village.ajouterHabitant(asterix);
+		
+		//Gaulois gaulois = village.trouverHabitant(1);
+		//System.out.println(gaulois);
+		//On a "null" en résultat car il ne se trouve rien à cette case, le gaulois se trouve dans le case 0
+		
+	}
+	
 }
